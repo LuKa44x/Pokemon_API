3ff1309c8d1cdf3540a23c488c9f57a785e440a3
LuKa44x <lucavicari44@gmail.com>
2026-04-14 17:22:50 +0200

    feat(search): implement user input for dynamic Pokémon fetching
    - Add input field and button to UI for Pokémon search
    - Handle click event to fetch Pokémon data
    - Remove prompt-based input in favor of DOM interaction
    - Render results dynamically using createCard.

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import { fetchPokemon } from "./fetch.js";
 import { createCard } from "./domManipulation.js";
-const pokemon = prompt("Enter the name of a Pokémon:");
-const data = await fetchPokemon(pokemon);
-console.log(data);
-createCard(data);
+const startBtn = document.querySelector(".pokemon-button");
+
+startBtn.addEventListener("click", async () => {
+  const pokemon = document
+    .getElementById("pokemon-input")
+    .value.trim()
+    .toLowerCase();
+
+  const data = await fetchPokemon(pokemon);
+  console.log(data);
+  createCard(data);
+});
